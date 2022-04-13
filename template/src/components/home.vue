cce0102bcb6b54b022610a893e2bb5b84d51cdf1
bigicy <15051418717@163.com>
2022-04-13 16:32:52 +0800

    模板首页

--- template/src/components/home.vue
+++ template/src/components/home.vue
@@ -1,12 +1,16 @@
 <template>
   <div class="home">
+    <van-nav-bar :title="title" :left-arrow="false" />
     <div>欢迎进入我的模板</div>
-    <van-nav-bar :title="title" />
   </div>
 </template>
 
 <script>
+import navbar from './navbar.vue'
 export default {
+  components:{
+    navbar
+  },
   data(){
     return {
       title:'模板',
